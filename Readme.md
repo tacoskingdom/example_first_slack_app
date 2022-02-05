5b42fc267aa5fbb3476ecbd4653331f4b222ffe1
taiichimotoi <tm050142@hotmail.co.jp>
2022-02-05 17:27:35 +0900

    Updated text in Readme

--- Readme.md
+++ Readme.md
@@ -3,3 +3,9 @@
 First Slack Bolt-js project for practice.
 
 [Official Website](https://github.com/slackapi/bolt-js)
+
+## Reference (Only Japanese available)
+
+[自宅Slackサーバーの作り方〜Bolt-js(Bolt for Javascript)で試す](https://deep.tacoskingdom.com/blog/155)
+
+[自宅Slackサーバーの作り方②〜Bolt-jsをソケットモード(WebSocket)で試す](https://deep.tacoskingdom.com/blog/156)
--- index.ts
+++ index.ts
@@ -1,10 +1,12 @@
 import { App } from '@slack/bolt';
 
 const app = new App({
-    signingSecret: process.env.SLACK_SIGNING_SECRET,
-    token: process.env.SLACK_BOT_TOKEN
-    // appToken: process.env.SLACK_APP_TOKEN,
-    // socketMode: true,
+    token: process.env.SLACK_BOT_TOKEN,
+    //👇通常モードを使う場合にコメントアウト
+    // signingSecret: process.env.SLACK_SIGNING_SECRET,
+    //👇ソケットモードを使う場合に設定するオプション
+    appToken: process.env.SLACK_APP_TOKEN,
+    socketMode: true,
 });
 
 app.message('hello', async ({ message, say }) => {
